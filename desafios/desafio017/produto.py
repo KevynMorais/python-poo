6f3863b943f88c93a3119b320f3f04f657a9fb70
Kevyn Morais <kevynmorais721@gmail.com>
2026-06-28 21:55:06 -0300

    Instanciamento de produtos
    Inclui a produção de painéis com a biblioteca Rich.

---
+++ desafios/desafio017/produto.py
@@ -0,0 +1,20 @@
+from rich import print
+from rich.panel import Panel
+
+class Produto:
+        
+    def __init__(self, nome = "Vazio", preco = 0):
+        self.modelo = nome
+        self.preco = preco
+
+    def etiqueta(self):
+        precof = f"R${self.preco:,.2f}"
+        caixa = Panel(f'{self.modelo:^30}{"-"*30}\n{precof:.^30}', title="Produto", height=5, width=35)
+        print(caixa)
+
+
+p1 = Produto("iPhone 17 Pro Max", 25_000.85)
+p2 = Produto("Notebook Gamer", 8_000)
+
+p1.etiqueta()
+p2.etiqueta()
